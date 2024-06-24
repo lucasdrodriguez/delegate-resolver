219324520263529b395ce7714ce3b2f63d95a428
lucasdrodriguez <lucas.d.rodriguez@gmail.com>
2024-06-24 10:19:44 -0300

    Add files via upload

---
+++ BingoUtn-resolver/Bingo/FrmBolillero.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Bingo
+{
+    partial class FrmBolillero
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_numero = new System.Windows.Forms.Button();
+            this.lb_salioNumero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_numero
+            // 
+            this.btn_numero.Location = new System.Drawing.Point(209, 30);
+            this.btn_numero.Name = "btn_numero";
+            this.btn_numero.Size = new System.Drawing.Size(291, 64);
+            this.btn_numero.TabIndex = 0;
+            this.btn_numero.Text = "Sacar proximo numero";
+            this.btn_numero.UseVisualStyleBackColor = true;
+            this.btn_numero.Click += new System.EventHandler(this.btn_numero_Click);
+            // 
+            // lb_salioNumero
+            // 
+            this.lb_salioNumero.AutoSize = true;
+            this.lb_salioNumero.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lb_salioNumero.Location = new System.Drawing.Point(26, 126);
+            this.lb_salioNumero.Name = "lb_salioNumero";
+            this.lb_salioNumero.Size = new System.Drawing.Size(0, 50);
+            this.lb_salioNumero.TabIndex = 1;
+            // 
+            // FrmBolillero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 263);
+            this.Controls.Add(this.lb_salioNumero);
+            this.Controls.Add(this.btn_numero);
+            this.Name = "FrmBolillero";
+            this.Text = "Bolillero";
+            this.Load += new System.EventHandler(this.FrmBolillero_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_numero;
+        private System.Windows.Forms.Label lb_salioNumero;
+    }
+}
+
---
+++ BingoUtn-resolver/Bingo/FrmBolillero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bingo
+{
+    public partial class FrmBolillero : Form
+    {
+        static Random rnd;
+        List<int> numeros;
+        List<int> numerosSalidos;
+
+        public FrmBolillero()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            numeros = new List<int>();
+            numerosSalidos = new List<int>();
+            SetearEstadoInicial();
+        }
+
+        private void SetearEstadoInicial()
+        {
+            numerosSalidos.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                numeros.Add(i);
+            }
+        }
+
+        private int ObtenerNumero()
+        {
+            int numeroQueSalio = rnd.Next(0, numeros.Count);
+
+            numeroQueSalio = numeros[numeroQueSalio];
+            
+            numeros.Remove(numeroQueSalio);
+            numerosSalidos.Add(numeroQueSalio);
+
+            return numeroQueSalio;
+        }
+
+        private void btn_numero_Click(object sender, EventArgs e)
+        {
+            int numero = ObtenerNumero();
+            lb_salioNumero.Text = $"Salio el numero: {numero}";
+        }
+
+        public void Ganador(string nombre)
+        {
+            MessageBox.Show("Ganador!!! : " + nombre);
+            this.btn_numero.Enabled = false;
+        }
+
+
+        private void FrmBolillero_Load(object sender, EventArgs e)
+        {
+
+            FrmCarton c1 = new FrmCarton("Jugador 1");
+            FrmCarton c2 = new FrmCarton("Jugador 2");
+            FrmCarton c3 = new FrmCarton("Jugador 3");
+
+            c1.Show();
+            c2.Show();
+            c3.Show();
+
+        }
+    }
+}
